e867504dcb10c9a060d4a9bea60b95121bb0fa7a
kahboom <rachelyordan@gmail.com>
2023-01-16 12:19:22 +0000

    fix(test): remove babel for  ts-jest + webpack instead; add jest config; fix module exports

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,22 +1,10 @@
-import { createRoot } from 'react-dom/client';
-import { screen } from '@testing-library/react';
+import { screen } from '@testing-library/dom';
+import { render } from '@testing-library/react';
+import { App } from './App';
 
-test('extension is published', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/A very awesome Step Extension./i);
-  expect(linkElement).toBeInTheDocument();
+test('renders learn react link', () => {
+ render(<App />);
+ const linkElement = screen.getByText(/A very awesome Step Extension/i);
+ expect(linkElement).toBeInTheDocument();
 });
-
-root.unmount();
-
-
-//import React from 'react';
-//import { render, screen } from '@testing-library/react';
-//import App from './App';
-
-//test('renders learn react link', () => {
-//  render(<App />);
-//  const linkElement = screen.getByText(/learn react/i);
-//  expect(linkElement).toBeInTheDocument();
-//});
 
